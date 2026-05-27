efb430b90bd04ca9f7bddde845751aeaafafc884
Kamalisuresh <kamalimeenashi@gmail.com>
2026-05-27 22:39:22 +0530

    fix: correct User model path casing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 // backend/controllers/userController.js
 
-const User = require("../models/User");
+const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
