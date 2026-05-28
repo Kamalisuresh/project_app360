aac957e6352fb1ace1d824ab581943dae2caa668
Kamalisuresh <kamalimeenashi@gmail.com>
2026-05-28 18:56:39 +0530

    mongoose port

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// backend/server.js
-
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,25 +7,32 @@ const alarmRoutes = require("./routes/alarmRoutes");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Connection
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
 .catch((err) => console.log(err));
 
+// Test Route
 app.get("/", (req,res)=>{
 
     res.send("Backend Running");
 
 });
 
+// Routes
 app.use("/api/users", userRoutes);
 
 app.use("/api/alarms", alarmRoutes);
 
-app.listen(5000,()=>{
+// PORT for Render
+const PORT = process.env.PORT || 5000;
 
-    console.log("Server running on port 5000");
+app.listen(PORT,()=>{
+
+    console.log(`Server running on port ${PORT}`);
 
 });
