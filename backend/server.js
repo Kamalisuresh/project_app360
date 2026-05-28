0c8832ff0b05734a88e9ca3a4429ae34883fe6fe
Kamalisuresh <kamalimeenashi@gmail.com>
2026-05-28 18:52:01 +0530

    mongoose

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+.catch((err) => console.log(err));
 
 app.get("/", (req,res)=>{
 
